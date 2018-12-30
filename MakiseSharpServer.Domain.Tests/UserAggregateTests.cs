3f2642608ec79612facc1ff66b0b5249f73b46cb
Unknown <k3nstin@gmail.com>
2018-12-30 21:41:52 +0100

    Add UserAggregate unit tests

---
+++ MakiseSharpServer.Domain.Tests/UserAggregateTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MakiseSharpServer.Domain.Entities.UserAggregate;
+using Xunit;
+
+namespace MakiseSharpServer.Domain.Tests
+{
+    public class UserAggregateTests
+    {
+        User user;
+        public UserAggregateTests()
+        {
+            user = new User(1, "a", "b");
+        }
+
+        [Fact]
+        public void User_ctr_should_not_accept_nulls()
+        {
+            Assert.Throws<ArgumentNullException>(() => new User(1, null, "xyz"));
+            Assert.Throws<ArgumentNullException>(() => new User(1, "xyz", null));
+        }
+
+        [Fact]
+        public void User_should_not_change_credentials_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => user.ChangeDiscordCredentials(null, "xyz"));
+            Assert.Throws<ArgumentNullException>(() => user.ChangeDiscordCredentials("xyz", null));
+        }
+
+        [Fact]
+        public void User_should_change_credentials()
+        {
+            //Arrange
+            const string accessToken = "abc";
+            const string refreshToken = "xyz";
+
+            //Act
+            user.ChangeDiscordCredentials(accessToken, refreshToken);
+
+            //Assert
+            Assert.Equal(accessToken, user.DiscordAccessToken);
+            Assert.Equal(refreshToken, user.DiscordRefreshToken);
+        }
+
+        [Fact]
+        public void User_add_refreshToken_success()
+        {
+            var currentTokens = user.AppRefreshTokens.Count;
+            user.AddRefreshToken();
+
+            Assert.Equal(currentTokens + 1, user.AppRefreshTokens.Count);
+        }
+    }
+}
