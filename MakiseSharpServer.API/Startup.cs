4eecf4bcbca5b183bbed44d820f6cc4e52829354
Unknown <k3nstin@gmail.com>
2019-02-11 11:11:20 +0100

    Bump MVC compatibility version to 3_0

--- MakiseSharpServer.API/Startup.cs
+++ MakiseSharpServer.API/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Reflection;
 using MakiseSharpServer.API.Swagger.Examples;
@@ -35,7 +35,7 @@ namespace MakiseSharpServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddSwaggerGen(c =>
             {
