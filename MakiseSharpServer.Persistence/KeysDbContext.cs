2d03cc6e705e88f7c53ab3a73875d93262f57b4b
Unknown <k3nstin@gmail.com>
2019-02-12 10:03:46 +0100

    Save DataProtection keys to db
    -KeysDbContext using Sqlite

---
+++ MakiseSharpServer.Persistence/KeysDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MakiseSharpServer.Persistence
+{
+    public class KeysDbContext : DbContext, IDataProtectionKeyContext
+    {
+        // A recommended constructor overload when using EF Core
+        // with dependency injection.
+        public KeysDbContext(DbContextOptions<KeysDbContext> options)
+            : base(options)
+        {
+        }
+
+        // This maps to the table that stores keys.
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+    }
+}
