91cf63c36d367504b6c57831c4f18863c2592ec9
Hebiame <19415637+Hebiame@users.noreply.github.com>
2019-02-13 12:44:41 +0100

    Add a Makise-themed UI (#10)

---
+++ MakiseSharpServer.Identity/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using IdentityServer4.Services;
+using MakiseSharpServer.Identity.Models;
+
+namespace MakiseSharpServer.Identity.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        private readonly IIdentityServerInteractionService interaction;
+        private readonly IHostingEnvironment environment;
+
+        public HomeController(IIdentityServerInteractionService interaction, IHostingEnvironment environment)
+        {
+            this.interaction = interaction;
+            this.environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Shows the error page
+        /// </summary>
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var vm = new ErrorViewModel();
+
+            // retrieve error details from identityserver
+            var message = await interaction.GetErrorContextAsync(errorId);
+            if (message != null)
+            {
+                vm.Error = message;
+
+                if (!environment.IsDevelopment())
+                {
+                    // only show in development
+                    message.ErrorDescription = null;
+                }
+            }
+
+            return View("Error", vm);
+        }
+    }
+}
