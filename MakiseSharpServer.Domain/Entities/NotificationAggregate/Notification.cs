0e20bec79502a22121cbf4dde3d0bc158b282496
Unknown <k3nstin@gmail.com>
2019-02-20 20:30:12 +0100

    Add NotificationAggregate

---
+++ MakiseSharpServer.Domain/Entities/NotificationAggregate/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MakiseSharpServer.Domain.SeedWork;
+
+namespace MakiseSharpServer.Domain.Entities.NotificationAggregate
+{
+    public class Notification : Entity, IAggregateRoot
+    {
+        public Webhook Webhook { get; private set; }
+
+        public Repository Repository { get; private set; }
+
+        public Guid AuthorId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the last build number for which a notification has been sent
+        /// </summary>
+        public uint LastBuildNumber { get; set; }
+    }
+}
---
+++ MakiseSharpServer.Persistence/EntityConfigurations/RepositoryEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MakiseSharpServer.Domain.Entities.NotificationAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MakiseSharpServer.Persistence.EntityConfigurations
+{
+    public class RepositoryEntityTypeConfiguration : IEntityTypeConfiguration<Repository>
+    {
+        public void Configure(EntityTypeBuilder<Repository> builder)
+        {
+            builder.Property(r => r.Slug).IsRequired();
+            builder.Property(r => r.RepoProvider).IsRequired();
+        }
+    }
+}
