e45853ac9557b30bfff43607c207cf3d6aa508ff
Unknown <k3nstin@gmail.com>
2019-02-26 20:40:46 +0100

    Encapsulate Notification properties

--- MakiseSharpServer.Domain/Entities/NotificationAggregate/Notification.cs
+++ MakiseSharpServer.Domain/Entities/NotificationAggregate/Notification.cs
@@ -5,6 +5,13 @@ namespace MakiseSharpServer.Domain.Entities.NotificationAggregate
 {
     public class Notification : Entity, IAggregateRoot
     {
+        public Notification(Webhook webhook, Repository repository, Guid authorId)
+        {
+            Webhook = webhook ?? throw new ArgumentNullException(nameof(webhook));
+            Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            AuthorId = authorId;
+        }
+
         public Webhook Webhook { get; private set; }
 
         public Repository Repository { get; private set; }
@@ -12,8 +19,8 @@ namespace MakiseSharpServer.Domain.Entities.NotificationAggregate
         public Guid AuthorId { get; private set; }
 
         /// <summary>
-        /// Gets or sets the last build number for which a notification has been sent
+        /// Gets the last build number for which a notification has been sent
         /// </summary>
-        public uint LastBuildNumber { get; set; }
+        public uint LastBuildNumber { get; private set; }
     }
 }
