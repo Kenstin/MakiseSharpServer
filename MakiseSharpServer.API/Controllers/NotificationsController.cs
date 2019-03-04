07457a804efec2cf43834214c2e16b8257692b74
Unknown <k3nstin@gmail.com>
2019-03-04 21:16:22 +0100

    Return Bad Request given wrong Discord OAuth2 code

--- MakiseSharpServer.API/Controllers/NotificationsController.cs
+++ MakiseSharpServer.API/Controllers/NotificationsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using MakiseSharpServer.API.Results;
@@ -19,12 +19,18 @@ namespace MakiseSharpServer.API.Controllers
         /// <returns>Created notification</returns>
         [HttpPost]
         [SwaggerResponse(400, "Requested status provider is not supported.")]
+        [SwaggerResponse(400, "Wrong Discord OAuth2 code provided.")]
         public async Task<ActionResult<NotificationCreatedDto>> CreateNotificationAsync([FromBody] CreateNotificationCommand command)
         {
             var result = await Mediator.Send(command);
             if (result.Errors.Any(e => e is StatusProviderNotSupportedError))
             {
                 return new MessageResult(400, "Requested status provider is not supported.");
+            }
+
+            if (result.Errors.Any(e => e is WrongCodeError))
+            {
+                return new MessageResult(400, "Wrong Discord OAuth2 code provided.");
             }
 
             throw new NotImplementedException();
