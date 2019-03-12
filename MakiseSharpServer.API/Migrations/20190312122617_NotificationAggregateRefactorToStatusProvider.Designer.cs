a6b57885ff5d9c3d0988f4d4ca426545a98d0f52
Unknown <k3nstin@gmail.com>
2019-03-12 13:29:33 +0100

    Refactor Notification to include StatusProvider

--- MakiseSharpServer.API/Migrations/20190312122617_NotificationAggregateRefactorToStatusProvider.Designer.cs
+++ MakiseSharpServer.API/Migrations/20190312122617_NotificationAggregateRefactorToStatusProvider.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MakiseSharpServer.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MakiseSharpServer.API.Migrations
 {
     [DbContext(typeof(MakiseDbContext))]
-    partial class MakiseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190312122617_NotificationAggregateRefactorToStatusProvider")]
+    partial class NotificationAggregateRefactorToStatusProvider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MakiseSharpServer.Domain/Entities/NotificationAggregate/Notification.cs
+++ MakiseSharpServer.Domain/Entities/NotificationAggregate/Notification.cs
@@ -5,16 +5,24 @@ namespace MakiseSharpServer.Domain.Entities.NotificationAggregate
 {
     public class Notification : Entity, IAggregateRoot
     {
-        public Notification(Webhook webhook, Repository repository, Guid authorId)
+        public Notification(Webhook webhook, string statusProvider, string slug, Guid authorId)
         {
             Webhook = webhook ?? throw new ArgumentNullException(nameof(webhook));
-            Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            StatusProvider = statusProvider ?? throw new ArgumentNullException(nameof(statusProvider));
+            Slug = slug ?? throw new ArgumentNullException(nameof(slug));
             AuthorId = authorId;
+        }
+
+        private Notification()
+        {
+        //used by EF Core
         }
 
         public Webhook Webhook { get; private set; }
 
-        public Repository Repository { get; private set; }
+        public string StatusProvider { get; private set; }
+
+        public string Slug { get; private set; }
 
         public Guid AuthorId { get; private set; }
 
